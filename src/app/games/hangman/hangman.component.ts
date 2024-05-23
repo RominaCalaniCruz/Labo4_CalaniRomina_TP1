a1904a83d6a4e161bcac86d39f007ce734732507
Romina Calani <cruznoemi220@gmail.com>
2024-05-22 22:36:55 -0300

    subiendo hangman corregido

--- src/app/games/hangman/hangman.component.ts
+++ src/app/games/hangman/hangman.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import Swal from 'sweetalert2';
 
@@ -10,7 +9,8 @@ import Swal from 'sweetalert2';
 })
 export class HangmanComponent {
   palabraOculta = '';
-  intentos = 0;
+  cantIntentos = 0;
+  cantIntentosMax = 7; //  7 imagenes disponibles por ahora
   palabra = '';
   gano = false;
   perdio = false;
@@ -18,77 +18,66 @@ export class HangmanComponent {
   botonesHabilitados: boolean[][];
   letrasTeclado: string[][] = [
     ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
-    ['J','K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q'],
-    ['R','S','T','U', 'V', 'W', 'X', 'Y', 'Z']
+    ['J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q'],
+    ['R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
   ];
-  
-  palabras = ['JAVASCRIPT', 'HTML', 'ANGULAR', 'SPAN', 'JUEGOS', 'APLICACION', 'TELETRABAJO', 'CELULAR', 'PANTALLA', 'COMPUTADORA'];
 
-  
-  
-  
+  palabrasJuego = ['JAVASCRIPT', 'HTML', 'ANGULAR',
+    'SPAN', 'JUEGOS', 'APLICACION', 'TELETRABAJO',
+    'CELULAR', 'PANTALLA', 'COMPUTADORA'];
+
   constructor() {
     this.inicializarValores();
-  
   }
 
-  inicializarValores(){
-    this.palabra = this.palabras[Math.floor(Math.random() * this.palabras.length)];
+  inicializarValores() {
+    this.palabra = this.palabrasJuego[Math.floor(Math.random() * this.palabrasJuego.length)];
     this.palabraOculta = '_ '.repeat(this.palabra.length).trimEnd();
     this.botonesHabilitados = this.letrasTeclado.map(row => row.map(() => false));
   }
 
-  comprobar(letra : string,fila: number, columna: number) {
-
+  comprobar(letra: string, fila: number, columna: number) {
     this.existeLetra(letra);
     const palabraOcultaArr = this.palabraOculta.split(' ');
     this.botonesHabilitados[fila][columna] = true;
-    
     for (let i = 0; i < this.palabra.length; i++) {
-
       if (this.palabra[i] === letra) {
         palabraOcultaArr[i] = letra;
       }
-
     }
-
     this.palabraOculta = palabraOcultaArr.join(' ');
-    this.verificaGane();
-
+    this.verificarAcierto();
   }
 
-  desactivarBoton(fila: number, columna: number){
+  desactivarBoton(fila: number, columna: number) {
     return this.botonesHabilitados[fila][columna];
   }
-  verificaGane() {
 
+  verificarAcierto() {
     const palabraArr = this.palabraOculta.split(' ');
     const palabraEvaluar = palabraArr.join('');
-
     if (palabraEvaluar === this.palabra) {
       this.gano = true;
       this.jugando = false;
-      console.log('Usuario GANO');
+      console.log('GANO');
     }
-
-    if (this.intentos >= 7) {
+    if (this.cantIntentos >= this.cantIntentosMax) {
       this.perdio = true;
       this.jugando = false;
-      console.log('Usuario perdio');
+      console.log('PERDIO');
     }
-
   }
-
 
   existeLetra(letra: string) {
     if (!this.palabra.includes(letra)) {
-      this.intentos++;
+      this.cantIntentos++;
     }
   }
-  reiniciarPartida(){
+
+  reiniciarPartida() {
     this.inicializarValores();
     this.jugando = true;
-    this.intentos = 0;
+    this.cantIntentos = 0;
     this.perdio = false;
     this.gano = false;
     Swal.fire({
